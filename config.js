0adcf47acfa17250ae3da5da64676cb5662418ac
zhangcz828 <zhangcz828@126.com>
2020-08-27 19:32:02 +0800

    Update config.js

--- config.js
+++ config.js
@@ -34,7 +34,7 @@ var config = {
     desc: {
         turn_on: "开灯",
         play: "音乐",
-        bannar_coming: "颜色",
+        bannar_coming: "装饰一下!",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
