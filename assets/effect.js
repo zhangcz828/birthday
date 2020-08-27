05f838a682d2e862722dc5c5f66f8be0c3a6f22b
zhangcz828 <zhangcz828@126.com>
2020-08-27 21:11:52 +0800

    Update effect.js

--- assets/effect.js
+++ assets/effect.js
@@ -159,8 +159,8 @@ $('document').ready(function(){
 		$('#b77').animate({top:240, left: vw+250},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
-		$(this).fadeOut('slow').delay(3000).promise().done(function(){
-			$('#story').fadeIn(6000);
+		$(this).fadeOut('slow').delay(9000).promise().done(function(){
+			$('#story').fadeIn(9000);
 		});
 	});
 	
